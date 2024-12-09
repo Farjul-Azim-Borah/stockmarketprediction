e77656d1cf76d1a7716f9c2b26e411e771313b28
Farjul_Azim_Borah <107451805+Farjul-Azim-Borah@users.noreply.github.com>
2024-12-09 19:20:52 +0530

    Update market.py

--- market.py
+++ market.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 import matplotlib.pyplot as plt
 import yfinance as yf
-from tensorflow.keras.models import load_model
+
 
 
 
